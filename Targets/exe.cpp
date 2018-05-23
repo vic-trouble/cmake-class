1d633b046f8d5bb7a8d130263364025d4f1f6ae5
Виктор Шульгин <victor.shulgin@playrix.com>
2018-05-23 23:00:57 +0300

    added targets

---
+++ Targets/exe.cpp
@@ -0,0 +1,9 @@
+#include "lib.h"
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "6 x 7 == " << MyLib::TheAnswer() << std::endl;
+	return 0;
+}
---
+++ Targets/lib.cpp
@@ -0,0 +1,6 @@
+#include "lib.h"
+
+int MyLib::TheAnswer()
+{
+	return 42;
+}
