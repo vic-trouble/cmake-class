ffebe5ee6c8836ad356596cdf65a69031f0aec3b
Виктор Шульгин <victor.shulgin@playrix.com>
2018-05-28 22:46:06 +0300

    numbered

--- Codegen/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "config.h"
-
-#include <iostream>
-
-int main()
-{
-	std::cout << "Built by " << buildAuthor << " on " << buildDate << std::endl;
-	return 0;
-}
--- Custom Targets/fail.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	std::cerr << "I'M A FAILURE :(" << std::endl;
-	return 1;
-}
